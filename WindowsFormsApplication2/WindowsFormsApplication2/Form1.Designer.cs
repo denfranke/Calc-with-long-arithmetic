4ee0c533da9a441132bd12d85fa0458b06baf585
Denis <33222627+denfranke@users.noreply.github.com>
2022-11-24 16:46:40 +0300

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class сравнение
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.t4 = new System.Windows.Forms.TextBox();
+            this.t5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.t6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.t7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.t8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(40, 83);
+            this.t1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(394, 26);
+            this.t1.TabIndex = 0;
+            // 
+            // t2
+            // 
+            this.t2.Location = new System.Drawing.Point(40, 29);
+            this.t2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(394, 26);
+            this.t2.TabIndex = 1;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(40, 306);
+            this.btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(320, 77);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "вычислить";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // t3
+            // 
+            this.t3.Location = new System.Drawing.Point(40, 180);
+            this.t3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(127, 26);
+            this.t3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 155);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Сравнение";
+            // 
+            // t4
+            // 
+            this.t4.Location = new System.Drawing.Point(202, 180);
+            this.t4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t4.Name = "t4";
+            this.t4.Size = new System.Drawing.Size(142, 26);
+            this.t4.TabIndex = 7;
+            // 
+            // t5
+            // 
+            this.t5.Location = new System.Drawing.Point(382, 180);
+            this.t5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t5.Name = "t5";
+            this.t5.Size = new System.Drawing.Size(182, 26);
+            this.t5.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 158);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Сумма";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(416, 158);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Разница";
+            // 
+            // t6
+            // 
+            this.t6.Location = new System.Drawing.Point(40, 243);
+            this.t6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t6.Name = "t6";
+            this.t6.Size = new System.Drawing.Size(126, 26);
+            this.t6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 218);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Умножение";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(226, 218);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Деление";
+            // 
+            // t7
+            // 
+            this.t7.Location = new System.Drawing.Point(202, 243);
+            this.t7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t7.Name = "t7";
+            this.t7.Size = new System.Drawing.Size(142, 26);
+            this.t7.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(416, 218);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Корень";
+            // 
+            // t8
+            // 
+            this.t8.Location = new System.Drawing.Point(382, 243);
+            this.t8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.t8.Name = "t8";
+            this.t8.Size = new System.Drawing.Size(181, 26);
+            this.t8.TabIndex = 16;
+            // 
+            // сравнение
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 402);
+            this.Controls.Add(this.t8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.t7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.t6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.t5);
+            this.Controls.Add(this.t4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "сравнение";
+            this.Text = "длинная арифметика";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox t4;
+        private System.Windows.Forms.TextBox t5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox t6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox t7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox t8;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class сравнение : Form
+    {
+        public сравнение()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            longint a = new longint(t1.Text);
+            longint b = new longint(t2.Text);
+
+            if (a < b)
+                t3.Text = "<";
+            else if (a > b)
+                t3.Text = ">";
+            else
+                t3.Text = "==";
+
+            longint k = a + b;
+            t4.Text = k.get();
+
+            if (a > b || (!(a < b) && !(a > b)))
+            {
+                k = a - b;
+                t5.Text = k.get();
+            }
+            else
+                if (a < b)
+            {
+                k = b - a;
+                string s = "-" + k.get();
+                t5.Text = s;
+            }
+
+            k = a * b;
+            t6.Text = k.get();
+
+            k = b / a;
+            while (k.Count() > 0 && k.getd(0) == 0)
+                k.DelAt(0);
+            t7.Text = k.getrev();
+
+            k = a.sqrt1();
+            t8.Text = k.get();
+        }
+    }
+
+    public class longint
+    {
+        List<int> digit = new List<int>();
+
+        bool neg = false;
+
+        public longint(longint a, longint b)
+        {
+            for (int i = a.Count() + b.Count(); i >= 0; i--)
+                digit.Add(0);
+        }
+
+        public longint() { }
+
+        public longint(string s)
+        {
+            if (s[0] != '-')
+            {
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    digit.Add(s[i] - '0');
+                }
+                neg = false;
+            }
+            else
+            {
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    digit.Add(s[i] - '0');
+                }
+                neg = true;
+            }
+        }
+
+        public string get()
+        {
+            string ans = "";
+            for (int i = digit.Count() - 1; i >= 0; i--)
+            {
+                ans += Convert.ToString(digit[i]);
+            }
+            return ans;
+        }
+
+        public string getrev()
+        {
+            string ans = "";
+            for (int i = 0; i < digit.Count(); i++)
+            {
+                ans += Convert.ToString(digit[i]);
+            }
+            return ans;
+        }
+
+        public int Count()
+        {
+            return digit.Count;
+        }
+
+        public void Add(int k)
+        {
+            digit.Add(k);
+        }
+
+        public int getd(int i)
+        {
+            if (i < digit.Count())
+                return digit[i];
+            return 0;
+        }
+
+        public void setd(int i, int k)
+        {
+            this.digit[i] = k;
+        }
+
+        public void DelAt(int i)
+        {
+            digit.RemoveAt(i);
+        }
+
+        public longint reverse()
+        {
+            longint a1 = new longint();
+            for (int i = digit.Count() - 1; i >= 0; i--)
+                a1.Add(digit[i]);
+            return a1;
+        }
+
+        public longint sqrt1()
+        {
+            longint a = this.copy();
+            a = a.reverse();
+            longint r = this.copy();
+            r = r.reverse();
+            longint l = new longint("0");
+            longint one = new longint("1");
+            longint two = new longint("2");
+            while (l < r)
+            {
+                longint m = (r + l).reverse() / two;
+                if (m * m < a)
+                    l = (m + one);
+                else
+                    r = m.copy();
+            }
+            if (r * r < a && r * r == a)
+                return r;
+            return l;
+        }
+
+        public longint copy()
+        {
+            longint a1 = new longint();
+            for (int i = 0; i < this.Count(); i++)
+                a1.Add(this.getd(i));
+            return a1;
+        }
+
+        public static longint operator +(longint a, longint b)
+        {
+            longint res = new longint();
+
+            int r = 0;
+            for (int i = 0; i < Math.Max(a.Count(), b.Count()); i++)
+            {
+                if (i < a.Count()) r += a.getd(i);
+                if (i < b.Count()) r += b.getd(i);
+                res.Add(r % 10);
+                r /= 10;
+            }
+            if (r > 0)
+                res.Add(r);
+            return res;
+        }
+
+        public static longint operator -(longint a, longint b)
+        {
+            longint res = new longint(a.get());
+            int r = 0;
+            for (int i = 0; i < b.Count() || r > 0; i++)
+            {
+                if (i >= res.Count())
+                    res.Add(0);
+                if (i < b.Count())
+                    res.digit[i] -= b.digit[i];
+                if (res.digit[i] < 0 && r == 0)
+                {
+                    res.digit[i] += 10;
+                    r = 1;
+                    continue;
+                }
+
+                if (r > 0)
+                {
+                    if (res.digit[i] > 0)
+                    {
+                        res.digit[i]--;
+                        r--;
+                    }
+                    else
+                        res.digit[i] += 9;
+                }
+            }
+            while (res.Count() > 1 && res.getd(res.Count() - 1) == 0)
+                res.DelAt(res.Count() - 1);
+            return res;
+        }
+
+        public static longint operator *(longint a, longint b)
+        {
+            longint c = new longint(a, b);
+            for (int i = 0; i < a.Count(); i++)
+            {
+                for (int j = 0, carry = 0; j < b.Count() || carry != 0; j++)
+                {
+                    Int64 sum;
+                    if (j < b.Count())
+                        sum = c.getd(i + j) + a.getd(i) * b.getd(j) + carry;
+                    else
+                        sum = c.getd(i + j) + a.getd(i) * 0 + carry;
+                    carry = (int)sum / 10;
+                    c.setd(i + j, (int)sum % 10);
+                }
+            }
+            while (c.Count() > 1 && c.getd(c.Count() - 1) == 0)
+                c.DelAt(c.Count() - 1);
+            return c;
+        }
+
+        public static longint operator /(longint a, longint b)
+        {
+            longint c = new longint();
+            longint res = new longint();
+            a = a.reverse();
+            b = b.reverse();
+            int i = 0;
+            while (a.Count() > i)
+            {
+                longint d = new longint();
+                int x = 0;
+                while (i < a.Count() && c.reverse() < b.reverse())
+                {
+                    c.Add(a.getd(i));
+                    if (x > 0)
+                        res.Add(0);
+                    x++;
+                    i++;
+                }
+                while (c.Count() > 0 && c.getd(0) == 0)
+                    c.DelAt(0);
+                int k = 0;
+                while (d.reverse() < c.reverse())
+                {
+                    d = (d.reverse() + b.reverse()).reverse();
+                    k++;
+                }
+                if ((d.reverse() < c.reverse()) || (d.reverse() > c.reverse()))
+                {
+                    d = (d.reverse() - b.reverse()).reverse();
+                    k--;
+                }
+                res.Add(k);
+                //  return res;
+                if (i == a.Count() && c.reverse() < b.reverse())
+                    break;
+                c = (c.reverse() - d.reverse()).reverse();
+                while (c.Count() > 0 && c.getd(0) == 0)
+                    c.DelAt(0);
+            }
+            return res;
+        }
+
+        public static bool operator <(longint a, longint b)
+        {
+            while (a.Count() > 0 && a.getd(a.Count() - 1) == 0)
+                a.DelAt(0);
+            while (b.Count() > 0 && b.getd(b.Count() - 1) == 0)
+                b.DelAt(0);
+            if (a.Count() != b.Count())
+                return a.Count() < b.Count();
+            for (int i = a.Count() - 1; i >= 0; --i)
+            {
+                if (a.digit[i] != b.digit[i])
+                    return a.digit[i] < b.digit[i];
+            }
+            return false;
+        }
+
+        public static bool operator >(longint a, longint b)
+        {
+            return b < a;
+        }
+    }
+}
+
